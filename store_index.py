427f615b92d70560cc92c96f5fde2fd302b05633
pubudu lasith <pubudu093@gmail.com>
2024-06-11 13:02:04 +1000

    store index added

---
+++ store_index.py
@@ -0,0 +1,40 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+
+# print(PINECONE_API_KEY)
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+
+index_name="medical-chatbot"
+
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud='aws', 
+            region='us-east-1'
+        ) 
+    ) 
+
+index = pc.Index(index_name)
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=PineconeVectorStore.from_documents(text_chunks, embeddings, index_name=index_name)
